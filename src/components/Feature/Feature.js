4c99a4fec34c8d4751865cb3384436469035d805
Charita Nichols <nichols.charita@gmail.com>
2020-06-06 04:46:38 -0400

    changes

---
+++ src/components/Feature/Feature.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function feature() {
+  return (
+    <form className="main__form">
+      <h2>Customize your laptop</h2>
+      {features}
+    </form>
+  );
+}
---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,24 @@
+import react from "react";
+
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default function summary(props) {
+  const summary = Object.keys(props.selected).map((feature, idx) => {
+    const featureHash = feature + "-" + idx;
+    const selectedOption = props.selected[feature];
+
+    return (
+      <div className="summary__option" key={featureHash}>
+        <div className="summary__option__label">{feature} </div>
+        <div className="summary__option__value">{selectedOption.name}</div>
+        <div className="summary__option__cost">
+          {USCurrencyFormat.format(selectedOption.cost)}
+        </div>
+      </div>
+    );
+  });
+  return summary;
+}
